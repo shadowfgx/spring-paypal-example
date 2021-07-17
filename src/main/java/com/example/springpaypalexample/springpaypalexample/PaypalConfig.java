1a07f515ddaa01fe81d90006283e26cf28981150
shadowekko <fernandogualan@outlook.com>
2021-07-18 01:16:44 +0200

    Configuracion de dependencias y properties

---
+++ src/main/java/com/example/springpaypalexample/springpaypalexample/PaypalConfig.java
@@ -0,0 +1,26 @@
+package com.example.springpaypalexample.springpaypalexample;
+
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class PaypalConfig {
+
+    @Value("${paypal.client.id}")
+    private String clientId;
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public Map<String,String> paypalSdkConfig(){
+        Map<String,String> configMap = new HashMap<>();
+        configMap.put("mode", mode);
+        return configMap;
+    }
+}
