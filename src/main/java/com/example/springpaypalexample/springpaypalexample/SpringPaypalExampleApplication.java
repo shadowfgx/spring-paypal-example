55e109c446dbb358f69d2a003dab055e2ac87256
shadowekko <fernandogualan@outlook.com>
2021-07-17 20:36:14 +0200

    Primer commit

---
+++ src/main/java/com/example/springpaypalexample/springpaypalexample/SpringPaypalExampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.springpaypalexample.springpaypalexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalExampleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringPaypalExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpaypalexample/springpaypalexample/SpringPaypalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpaypalexample.springpaypalexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaypalExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
